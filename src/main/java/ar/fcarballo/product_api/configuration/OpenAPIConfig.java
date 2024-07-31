e536d03c3e11b4188358ca0d5ad05b0f8d027a7f
Fernando Carballo <fcarballo@gmail.com>
2024-07-31 03:20:11 -0300

    https

--- src/main/java/ar/fcarballo/product_api/configuration/OpenAPIConfig.java
+++ src/main/java/ar/fcarballo/product_api/configuration/OpenAPIConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.hateoas.Links;
 import org.springframework.web.filter.DelegatingFilterProxy;
+import org.springframework.web.filter.ForwardedHeaderFilter;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -15,12 +16,11 @@ import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
 
 @Configuration
-public class OpenAPIConfig
-{
+public class OpenAPIConfig {
 
-	/* 
+	/*
 	 * This bean is used to register the swaggerBasicAuthFilter bean.
-	*/
+	 */
 	@Bean
 	public FilterRegistrationBean<DelegatingFilterProxy> swaggerBasicAuthFilterRegistration() {
 		FilterRegistrationBean<DelegatingFilterProxy> registrationBean = new FilterRegistrationBean<>();
@@ -33,13 +33,10 @@ public class OpenAPIConfig
 	 * This bean is used to configure the CORS.
 	 */
 	@Bean
-	public WebMvcConfigurer corsConfigurer()
-	{
-		return new WebMvcConfigurer()
-		{
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
 			@Override
-			public void addCorsMappings(CorsRegistry registry)
-			{
+			public void addCorsMappings(CorsRegistry registry) {
 				registry.addMapping("/**")
 						.allowedOrigins("*")
 						.allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH")
@@ -53,8 +50,7 @@ public class OpenAPIConfig
 	 * This bean is used to configure the OpenAPI info.
 	 */
 	@Bean
-	public OpenAPI myOpenAPI()
-	{
+	public OpenAPI myOpenAPI() {
 
 		SpringDocUtils.getConfig().addResponseTypeToIgnore(Links.class);
 		Contact contact = new Contact();
@@ -64,8 +60,14 @@ public class OpenAPIConfig
 
 		License mitLicense = new License().name("MIT License").url("https://choosealicense.com/licenses/mit/");
 
-		Info info = new Info().title("Productos REST API").version("1.0").contact(contact).description("Esta API permite crear, modificar y consultar productos.").license(mitLicense);
+		Info info = new Info().title("Productos REST API").version("1.0").contact(contact)
+				.description("Esta API permite crear, modificar y consultar productos.").license(mitLicense);
 
 		return new OpenAPI().info(info);
+	}
+
+	@Bean
+	public ForwardedHeaderFilter forwardedHeaderFilter() {
+		return new ForwardedHeaderFilter();
 	}
 }
